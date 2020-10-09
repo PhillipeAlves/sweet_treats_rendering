3dcfd403c9e1b04d54c8e5b08745c60c4806e0eb
PhillipeAlves <phillipe.alves@gmail.com>
2020-10-09 14:09:50 +1100

    redirect

--- src/client/pages/HomePage.js
+++ src/client/pages/HomePage.js
@@ -1,34 +1,20 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { connect } from "react-redux";
+import React from 'react'
 
-const Home = ({ auth }) => {
-  const text = auth ? (
-    <Link
-      style={{ fontSize: "3rem" }}
-      className='inner-ui-card treats-link'
-      to='/recipes'
-    >
-      SWEET TREATS
-    </Link>
-  ) : (
-    <div className='inner-ui-card'>
-      <h2>Welcome to</h2>
-      <h1>SWEET TREATS</h1>
-      <p className='login-text'>Log in to get amazing cooking recipes.</p>
-      <a className='login-btn' href='/api/auth/google'>
-        <i className='fab fa-google'> </i> Login
-      </a>
+const Home = () => {
+  return (
+    <div className='ui-card gold-border'>
+      <div className='inner-ui-card'>
+        <h2>Welcome to</h2>
+        <h1>SWEET TREATS</h1>
+        <p className='login-text'>Log in to get amazing cooking recipes.</p>
+        <a className='login-btn' href='/api/auth/google'>
+          <i className='fab fa-google'></i> Login
+        </a>
+      </div>
     </div>
-  );
-
-  return <div className='ui-card gold-border'>{text}</div>;
-};
-
-function mapStateToProps({ auth }) {
-  return { auth };
+  )
 }
 
 export default {
-  component: connect(mapStateToProps)(Home),
-};
+  component: Home
+}
